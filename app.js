5996935af37c3215e11d177d0a824cbc27a094fd
Alessio Biancalana <dottorblaster@gmail.com>
2014-09-17 00:55:01 +0200

    first very true impl

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
-var Hapi = require('hapi');
-var server = new Hapi.Server(3000);
+var Hapi = require('hapi'),
+	server = new Hapi.Server(3000);
+
+server.route({
+	method: 'GET',
+	path: '/',
+	handler: function(request, reply){
+		reply('stocazzo');
+	}
+});
+
+server.route({
+	method: 'GET',
+	path: '/caps',
+	handler: function(request, reply){
+		reply('stocazzo'.toUpperCase());
+	}
+});
+
+server.route({
+	method: 'GET',
+	path: '/camel',
+	handler: function(request, reply){
+		reply('StoCazzo');
+	}
+});
 
 server.start(function () {
-  console.log('stocazzo');
+	console.log('A stocazzo provider is running at: ', server.info.uri);
 });
