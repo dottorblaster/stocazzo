3f3d12ca998db6f5bd918388e96cd1d1d42876e7
Alessio Biancalana <dottorblaster@gmail.com>
2015-07-20 20:28:46 +0200

    refactored unit tests

--- test/test.js
+++ test/test.js
@@ -1,29 +1,31 @@
-var Lab = require('lab');
-var server = require('../app.js');
+var Lab = require('lab'),
+	Code = require('code'),
+	server = require('../app.js').server,
+	lab = exports.lab = Lab.script();
 
-Lab.experiment("Testing di stocazzo", function() {
-	Lab.test("GET /", function(done) {
+lab.experiment("Testing di stocazzo", function() {
+	lab.test("GET /", function(done) {
 		var options = {
 			method: 'GET',
 			url: '/'
 		};
 
 		server.inject(options, function(response) {
-			Lab.expect(response.statusCode).to.equal(200);
-			Lab.expect(response.result.response).to.equal('stocazzo');
+			Code.expect(response.statusCode).to.equal(200);
+			Code.expect(JSON.parse(response.result).response).to.equal('stocazzo');
 			done();
 		});
 	});
 
-	Lab.test("GET /caps", function(done) {
+	lab.test("GET /caps", function(done) {
 		var options = {
 			method: 'GET',
 			url: '/caps'
 		};
 
 		server.inject(options, function(response) {
-			Lab.expect(response.statusCode).to.equal(200);
-			Lab.expect(response.result.response).to.equal('STOCAZZO');
+			Code.expect(response.statusCode).to.equal(200);
+			Code.expect(JSON.parse(response.result).response).to.equal('STOCAZZO');
 			done();
 		});
 	});
