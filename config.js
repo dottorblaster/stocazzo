727f76d8676cdf3b475e3f1ae31cc6d5d3cb82b2
Alessio Biancalana <dottorblaster@gmail.com>
2019-08-10 18:20:34 +0200

    Partial rewrite to make the software compatible with hapi 18

--- config.js
+++ config.js
@@ -1,32 +1,32 @@
 var config = {
-	sc: 'stocazzo',
-	port: 80,
-	routes: {
-		"root": {
-			value: "stocazzo",
-			big: "gran"
-		},
-		"caps": {
-			value: "STOCAZZO",
-			big: "GRAN"
-		},
-		"camel": {
-			value: "StoCazzo",
-			big: "Gran"
-		},
-		"ascii": {
-			value: "8====D",
-			big: "==="
-		},
-		"underscore": {
-			value: "sto_cazzo",
-			big: "_gran"
-		},
-		"sto-conte": {
-			value: "Sto cazzo!",
-			big: " gran"
-		}
-	}
+  sc: "stocazzo",
+  port: 3000,
+  routes: {
+    root: {
+      value: "stocazzo",
+      big: "gran"
+    },
+    caps: {
+      value: "STOCAZZO",
+      big: "GRAN"
+    },
+    camel: {
+      value: "StoCazzo",
+      big: "Gran"
+    },
+    ascii: {
+      value: "8====D",
+      big: "==="
+    },
+    underscore: {
+      value: "sto_cazzo",
+      big: "_gran"
+    },
+    "sto-conte": {
+      value: "Sto cazzo!",
+      big: " gran"
+    }
+  }
 };
 
 module.exports = config;
