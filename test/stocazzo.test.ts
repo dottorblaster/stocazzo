da169e901dea97606648143c2f5aed4454b5e229
Alessio Biancalana <dottorblaster@gmail.com>
2020-07-18 17:13:30 +0200

    Reformat source with Prettier

--- test/stocazzo.test.ts
+++ test/stocazzo.test.ts
@@ -1,4 +1,4 @@
-import { init } from '../lib/server';
+import { init } from "../lib/server";
 
 describe("Testing stocazzo's", () => {
   let server;
@@ -7,115 +7,115 @@ describe("Testing stocazzo's", () => {
     server = await init();
   });
 
-  it('GET /', async () => {
+  it("GET /", async () => {
     const options = {
-      method: 'GET',
-      url: '/',
+      method: "GET",
+      url: "/",
     };
 
     const response = await server.inject(options);
     expect(response.statusCode).toBe(200);
-    expect(response.json().response).toBe('stocazzo');
+    expect(response.json().response).toBe("stocazzo");
   });
 
-  it('GET / with request body', async () => {
+  it("GET / with request body", async () => {
     const options = {
-      method: 'GET',
-      url: '/?q=chi%20e',
+      method: "GET",
+      url: "/?q=chi%20e",
     };
 
     const response = await server.inject(options);
     expect(response.statusCode).toBe(200);
-    expect(response.json().query).toBe('chi e?');
-    expect(response.json().response).toBe('stocazzo');
+    expect(response.json().query).toBe("chi e?");
+    expect(response.json().response).toBe("stocazzo");
   });
 
-  it('GET /caps', async () => {
+  it("GET /caps", async () => {
     const options = {
-      method: 'GET',
-      url: '/caps',
+      method: "GET",
+      url: "/caps",
     };
 
     const response = await server.inject(options);
     expect(response.statusCode).toBe(200);
-    expect(response.json().response).toBe('STOCAZZO');
+    expect(response.json().response).toBe("STOCAZZO");
   });
 
-  it('GET /caps with request body', async () => {
+  it("GET /caps with request body", async () => {
     const options = {
-      method: 'GET',
-      url: '/caps?q=chi%20e',
+      method: "GET",
+      url: "/caps?q=chi%20e",
     };
 
     const response = await server.inject(options);
     expect(response.statusCode).toBe(200);
-    expect(response.json().query).toBe('chi e?');
-    expect(response.json().response).toBe('STOCAZZO');
+    expect(response.json().query).toBe("chi e?");
+    expect(response.json().response).toBe("STOCAZZO");
   });
 
-  it('GET /camel', async () => {
+  it("GET /camel", async () => {
     const options = {
-      method: 'GET',
-      url: '/camel',
+      method: "GET",
+      url: "/camel",
     };
 
     const response = await server.inject(options);
     expect(response.statusCode).toBe(200);
-    expect(response.json().response).toBe('StoCazzo');
+    expect(response.json().response).toBe("StoCazzo");
   });
 
-  it('GET /ascii', async () => {
+  it("GET /ascii", async () => {
     const options = {
-      method: 'GET',
-      url: '/ascii',
+      method: "GET",
+      url: "/ascii",
     };
 
     const response = await server.inject(options);
     expect(response.statusCode).toBe(200);
-    expect(response.json().response).toBe('8====D');
+    expect(response.json().response).toBe("8====D");
   });
 
-  it('GET /underscore', async () => {
+  it("GET /underscore", async () => {
     const options = {
-      method: 'GET',
-      url: '/underscore',
+      method: "GET",
+      url: "/underscore",
     };
 
     const response = await server.inject(options);
     expect(response.statusCode).toBe(200);
-    expect(response.json().response).toBe('sto_cazzo');
+    expect(response.json().response).toBe("sto_cazzo");
   });
 
-  it('GET /underscore with big=true', async () => {
+  it("GET /underscore with big=true", async () => {
     const options = {
-      method: 'GET',
-      url: '/underscore?big=1',
+      method: "GET",
+      url: "/underscore?big=1",
     };
 
     const response = await server.inject(options);
     expect(response.statusCode).toBe(200);
-    expect(response.json().response).toBe('sto_gran_cazzo');
+    expect(response.json().response).toBe("sto_gran_cazzo");
   });
 
-  it('GET /sto-conte', async () => {
+  it("GET /sto-conte", async () => {
     const options = {
-      method: 'GET',
-      url: '/sto-conte',
+      method: "GET",
+      url: "/sto-conte",
     };
 
     const response = await server.inject(options);
     expect(response.statusCode).toBe(200);
-    expect(response.json().response).toBe('Sto cazzo!');
+    expect(response.json().response).toBe("Sto cazzo!");
   });
 
-  it('GET /sto-conte with big=true', async () => {
+  it("GET /sto-conte with big=true", async () => {
     const options = {
-      method: 'GET',
-      url: '/sto-conte?big=1',
+      method: "GET",
+      url: "/sto-conte?big=1",
     };
 
     const response = await server.inject(options);
     expect(response.statusCode).toBe(200);
-    expect(response.json().response).toBe('Sto gran cazzo!');
+    expect(response.json().response).toBe("Sto gran cazzo!");
   });
 });
