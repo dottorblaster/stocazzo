52b553a575aba6f843816dd21a9150d1194ced7e
Alessio Biancalana <dottorblaster@gmail.com>
2020-08-25 18:09:10 +0200

    Decorate the whole service with type annotations

--- lib/server.ts
+++ lib/server.ts
@@ -1,10 +1,10 @@
-import fastify from "fastify";
+import fastify, { FastifyInstance } from "fastify";
 import cors from "fastify-cors";
 import { pipe } from "@drblaster/pipe";
 
 import { requestFormatter, dilate, parsePort } from "./utils";
 import config from "./config";
-import { StocazzoRequest } from "./types";
+import { StocazzoRequest, StocazzoResponse } from "./types";
 
 const sticazzi = config.routes;
 
@@ -12,7 +12,7 @@ const server = fastify();
 
 server.register(cors);
 
-server.get<StocazzoRequest>("/:format", (request, reply) => {
+server.get("/:format", (request: StocazzoRequest, reply) => {
   const {
     params: { format },
   } = request;
@@ -20,7 +20,7 @@ server.get<StocazzoRequest>("/:format", (request, reply) => {
   const obj = fallback ? sticazzi.root : sticazzi[format];
   const response = { response: obj.value };
 
-  const processedResponse = pipe(
+  const processedResponse: StocazzoResponse = pipe(
     requestFormatter(request),
     dilate(request)(obj.big)
   )(response);
@@ -28,12 +28,12 @@ server.get<StocazzoRequest>("/:format", (request, reply) => {
   reply.send(processedResponse);
 });
 
-export const init = async () => {
+export const init = async (): Promise<FastifyInstance> => {
   await server.ready();
   return server;
 };
 
-export const start = async () => {
+export const start = async (): Promise<FastifyInstance> => {
   const url = await server.listen(parsePort(process.env.PORT), "0.0.0.0");
   console.log(`A stocazzo provider is running at: ${url}`);
   return server;
