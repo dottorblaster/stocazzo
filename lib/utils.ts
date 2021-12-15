33a9de68d8347ace207d3ce637207b56039b1363
Alessio Biancalana <dottorblaster@gmail.com>
2021-12-15 22:23:14 +0100

    Update varg definitions and add tests

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,34 +1,37 @@
+import { substr } from 'runes';
 import { StocazzoRequest, StocazzoResponse } from './types';
 
 type Port = string | number;
 
-export const requestFormatter = (req: StocazzoRequest) => (
-  res: StocazzoResponse
-): StocazzoResponse => {
-  if (req.query.q) {
-    res.query = req.query.q;
-    if (res.query.slice(-1) != '?') {
-      res.query = res.query + '?';
-    }
-  }
-  return { ...res };
-};
-
-export const dilate = (req: StocazzoRequest) => (s: string) => (
-  res: StocazzoResponse
-): StocazzoResponse => {
-  const { response } = res;
-
-  return req.query.big == 1
-    ? {
-        ...res,
-        response: `${response.substr(0, 3)}${s}${response.substr(
-          3,
-          response.length - 1
-        )}`,
+export const requestFormatter =
+  (req: StocazzoRequest) =>
+  (res: StocazzoResponse): StocazzoResponse => {
+    if (req.query.q) {
+      res.query = req.query.q;
+      if (res.query.slice(-1) != '?') {
+        res.query = res.query + '?';
       }
-    : { ...res };
-};
+    }
+    return { ...res };
+  };
+
+export const dilate =
+  (req: StocazzoRequest) =>
+  (s: string) =>
+  (res: StocazzoResponse): StocazzoResponse => {
+    const { response } = res;
+
+    return req.query.big == 1
+      ? {
+          ...res,
+          response: `${substr(response, 0, 3)}${s}${substr(
+            response,
+            3,
+            response.length - 1
+          )}`,
+        }
+      : { ...res };
+  };
 
 export const parsePort = (envPort: Port = 3000): number => {
   const port = typeof envPort === 'string' ? Number(envPort) : envPort;
