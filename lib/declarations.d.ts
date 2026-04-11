6e4ecf22198ae08aa840835f7290b45bb898b8c2
Alessio Biancalana <alessio@dottorblaster.it>
2026-04-11 13:22:00 +0200

    chore: add @types/runes to avoid writing a dts

--- lib/declarations.d.ts
+++ lib/declarations.d.ts
@@ -1,7 +1,3 @@
-declare module 'runes' {
-  export function substr(str: string, start: number, length: number): string;
-}
-
 declare module '@drblaster/pipe' {
   export function pipe<T>(...fns: Array<(arg: T) => T>): (arg: T) => T;
 }
